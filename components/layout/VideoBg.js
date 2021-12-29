f1c5907e5643d101f386280339a67f1c650a29a4
Sirat <sirat.b99@gmail.com>
2021-12-29 22:23:10 +0530

    added a video background! figure out how to compress it further compression can be utilized for .hdr/.hdri environment texture files

---
+++ components/layout/VideoBg.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+
+const Video = styled.video`
+   object-fit: cover;
+   width: 100%;
+   height: 100%;
+   position: fixed;
+   z-index: -1;
+   /* filter: contrast(2) brightness(0.35); */
+`;
+
+const VideoBg = () => {
+   const videoRef = useRef();
+
+   useEffect(() => {
+      setTimeout(() => {
+         videoRef.current.play();
+      }, 5000);
+   }, []);
+
+   return (
+      <Video ref={videoRef} loop autoPlay muted>
+         <source src="/media/clouds.mp4" type="video/mp4" />
+      </Video>
+   );
+};
+
+export default VideoBg;
