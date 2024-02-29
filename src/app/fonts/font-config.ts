b1f825233cd5fee227c9c27b7866c13090120c35
✿(˘̩◡˘̩♡) Sirat <sirat.b99@gmail.com>
2024-02-29 17:18:56 +0530

    💅 styles - removed app/page.tsx boilerplate, added local fonts to replace inter

---
+++ src/app/fonts/font-config.ts
@@ -0,0 +1,51 @@
+import localFont from "next/font/local";
+
+export const authenticSans = localFont({
+  src: [
+    {
+      path: "./authentic-sans/AUTHENTICSans-60.woff",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./authentic-sans/AUTHENTICSans-90.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./authentic-sans/AUTHENTICSans-130.woff",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./authentic-sans/AUTHENTICSans-150.woff",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
+
+export const authenticSansCondensed = localFont({
+  src: [
+    {
+      path: "./authentic-sans/AUTHENTICSans-Condensed-60.woff",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./authentic-sans/AUTHENTICSans-Condensed-90.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./authentic-sans/AUTHENTICSans-Condensed-130.woff",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./authentic-sans/AUTHENTICSans-Condensed-150.woff",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
