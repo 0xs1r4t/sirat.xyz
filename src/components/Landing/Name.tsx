b8a728076a1889e5a15e95c6ccf649b03cadf13b
✿(˘̩◡˘̩♡) Sirat <sirat.b99@gmail.com>
2025-01-28 23:24:13 +0530

    🗨️ fixed the text stroke forthe Name component

--- src/components/Landing/Name.tsx
+++ src/components/Landing/Name.tsx
@@ -4,10 +4,10 @@ const Name = () => {
   return (
     <div
       aria-label="page title"
-      className="relative font-name px-1.5 py-0.5 italic text-center text-7xl text-foreground text-stroke-3 text-stroke-foreground not-prose sm:text-9xl sm:pl-2.5 sm:pt-1"
+      className="relative font-name pl-1.5 pt-0.5 italic text-center text-7xl text-foreground text-stroke-3 text-stroke-foreground not-prose sm:text-9xl sm:pl-2.5 sm:pt-1"
     >
       0xS1R4T
-      <span className="absolute top-0 left-0 w-full text-muted-200 text-stroke-1">
+      <span className="absolute top-0 left-0 w-full text-muted-200 text-stroke-foreground text-stroke-1 sm:text-stroke-1 sm:text-stroke-foreground">
         0xS1R4T
       </span>
     </div>
